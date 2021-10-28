b08195c294c01c30a08b43f42239c48627382c74
danG02 <45547767+danG02@users.noreply.github.com>
2021-10-28 12:11:56 -0400

    Updated Codes, comments, and fixed bugs

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const bioBox = document.querySelector('#container')
-const triggerFlip = document.querySelector('#about')
-const pro = document.querySelector('#projects')
+const bioBox = document.querySelector('#container');
+const bioBox2 = document.querySelector('.MainContainer');
+const triggerFlip = document.querySelector('#about');
 
 //document.getElementById("about").addEventListener("click", function() {
 //    alert("Hello World!")
@@ -8,45 +8,7 @@ const pro = document.querySelector('#projects')
 
 
 triggerFlip.addEventListener('click', function() {
-    bioBox.classList.toggle('flipped')
-})
+    bioBox.classList.toggle('flipped');
+}, false);
 
-
-
-
-//this makes the container flip once -_-
-/*
-triggerFlip.addEventListener("click", function() {
-    document.getElementById('container').style.transform = "rotateY(180deg)"
-})
-
-const flip = (bioBox) => {
-    bioBox.target.style.transform = "rotateY(180deg)"
-}
-triggerFlip.addEventListener('click', flip, true)
-
-document.getElementById('about').addEventListener('click', function() {
-    document.getElementById('container').style.transform = "rotateY(180deg)"
-})
-
-*/
-
-
-//element.addEventListener("click", function(){ alert("Hello World!"); });
-//document.getElementById(id).style.property = new style
-
-//$('.card').click(function(){
-//    $(this).toggleClass('flipped');
-//});
-//('about').on('click', function () {
-//    ('container').toggleClass('flipped');
-//  });
-
-/*
-This is how to click and flip every time
-var card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
-*/
 
